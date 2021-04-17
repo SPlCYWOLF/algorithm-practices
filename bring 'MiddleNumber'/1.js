4e7709ba39fb49edf78625116d0bfac1a7f934b4
Tae Hun KIM <gift2barry@gmail.com>
2021-04-17 16:06:39 +0900

    14th try

--- bring 'MiddleNumber'/1.js
+++ bring 'MiddleNumber'/1.js
@@ -1,20 +1,16 @@
 const a = "123456";
 const stringLength = a.length;
 const evenOrOdd = stringLength % 2;
-const middleStrEven = (stringLength / 2) - 1;
 const middleStrOdd = stringLength / 2;
-
+const middleStrEven = middleStrOdd + 1;
 
 function answer(a) {
     if(evenOrOdd === 0) {
-        return [a.charAt(middleStrEven), a.charAt(middleStrOdd)];
+        return a.substring(middleStrOdd - 1, middleStrEven);
     } else {
         return a.charAt(middleStrOdd);
     }
 }
 
-
-
-console.log(`middle letter is ${answer(a)}`);
-
-console.log(`string length is ${stringLength}`);
+console.log(`middle letter is "${answer(a)}"`);
+console.log(`string length is "${stringLength}"`);
