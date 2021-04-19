a124a84910e23424e6f925d60c346689e64dd7da
Tae Hun KIM <gift2barry@gmail.com>
2021-04-19 18:53:00 +0900

    6th try

--- waterlemon/1.js
+++ waterlemon/1.js
@@ -1,9 +1,22 @@
 const n = 5;
+var words = [];
 
-for(var i=0; i < n; i++) {
-  if(i % 2 === 0) {
-    console.log("수");
-  } else {
-    console.log("박");
-  }
+function solution() {
+  for (var i=0; i < n; i++) {
+    if(i % 2 === 0) {
+      words.push("수");
+    } else {
+      words.push("박");
+    }
 }
+  console.log(words);
+}
+
+solution();
+
+
+
+
+
+/*길이가 n이고, "수박수박수박수...."와 같은 패턴을 유지하는 문자열을 리턴하는 함수, solution을 완성하세요. 
+  예를들어 n이 4이면 "수박수박"을 리턴하고 3이라면 "수박수"를 리턴하면 됩니다.
