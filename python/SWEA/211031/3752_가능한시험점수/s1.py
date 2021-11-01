53158c9f40c0b94dc0510bfac13f456fe7a376de
KIM Tae Hun <gift2barry@gmail.com>
2021-11-01 23:56:56 +0900

    211101 algorithm ver.1

---
+++ python/SWEA/211031/3752_가능한시험점수/s1.py
@@ -0,0 +1,64 @@
+import sys
+sys.stdin = open('input.txt')
+
+
+'''
+itertools 의 combination 활용해보려 했지만 30분 투자해도 안되서 포기
+dfs 활용하여 각 부분집합 요소들의 원소들의 합 구하기
+
+
+'''
+
+
+# def dfs(k):
+#
+#     if k == N:
+#         return
+#
+#     for i in range(k, N):
+#         if not visited[i]:
+#             visited[i] = 1
+#             tmp[k] = nums[i]
+#             score = sum(tmp)
+#             ans.add(score)
+#             dfs(k+1)
+#             visited[i] = 0
+#             tmp[k] = 0
+# -------------------------------------------------
+# def dfs(score, k):
+#
+#     if k == N:
+#         return
+#
+#     else:
+#         for i in range(k, N):
+#             if not visited[i]:
+#                 visited[i] = 1
+#                 ans.add(score+nums[i])
+#                 dfs(score+nums[i], k+1)
+#                 visited[i] = 0
+
+def dfs(score, k):            # 중복조합
+    if k == N:
+        return
+
+    else:
+        for i in range(k, N):
+            if not visited[i]:
+                visited[i] = 1
+                ans.add(score+nums[i])
+                dfs(score+nums[i], k+1)
+                visited[i] = 0
+
+
+for tc in range(1, int(input())+1):
+
+    N = int(input())
+    nums = tuple(map(int, input().split()))
+    tmp = [0] * N
+    visited = [0] * N
+    ans = set()
+    
+    dfs(0, 0)    # 점수 배점들, 현재 점수, dfs 깊이
+    # print('#{} {}'.format(tc, len(ans)+1))
+    print(ans)
