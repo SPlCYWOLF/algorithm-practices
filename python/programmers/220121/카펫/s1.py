73168fab5a12b968a3a879b7c630270ec8d49bcf
KIM Tae Hun <gift2barry@gmail.com>
2022-01-24 23:47:03 +0900

    220124 algorithm incomplete

--- python/programmers/220121/카펫/s1.py
+++ python/programmers/220121/카펫/s1.py
@@ -1,20 +1,20 @@
-# 패작
+# 패작 음 도무지 모르겠다
 
 def solution(brown, yellow):
     # 1. 총 블록 갯수를 기준으로
     temp = brown + yellow
-    gongyaksu = []
+    yaksu = []
     # 2. 모든 공약수 담아서
     for i in range(1, temp+1):
         if temp % i == 0:
-            gongyaksu.append(i)
+            yaksu.append(i)
     
-    mid = len(gongyaksu) // 2
+    mid = len(yaksu) // 2
     # 3. 해당 공약수 리스트의 가운데 두 숫자가 정답.
     # 4. 공약수 개수가 홀수 인 경우는 가운데 숫자 두개가 정답?
-    if len(gongyaksu) % 2:
-        answer = gongyaksu[mid:mid+1] * 2
+    if len(yaksu) % 2:
+        answer = yaksu[mid:mid+1] * 2
     else:
-        answer = sorted(gongyaksu[mid-1:mid+1], reverse=True)
+        answer = sorted(yaksu[mid-1:mid+1], reverse=True)
 
     return answer
