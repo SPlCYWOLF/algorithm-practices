040d7358cfcd8a019b444e33ce2ef098aaeff9f8
KIM Tae Hun <gift2barry@gmail.com>
2022-03-08 13:18:26 +0900

    vanillajs: Delete legacy folder

--- vanilla-js/finding'KIM'/1.js
+++
@@ -1,9 +0,0 @@
-const seoul = ["khan", "ken", "Kim", "wow", "hahahhdih"];
-
-function solution(seoul) {
-    var answer = seoul.indexOf("Kim");
-    return `김서방은 ${answer}에 있다`;
-}
-
-console.log(solution(seoul));
-
