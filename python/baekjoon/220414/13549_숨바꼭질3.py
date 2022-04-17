eb41c3ea59d3a66b7c367bc38946de590ffbd54a
KIM Tae Hun <gift2barry@gmail.com>
2022-04-17 21:24:24 +0900

    algorithm: Fix 13549_숨바꼭질3 complete

--- python/baekjoon/220414/13549_숨바꼭질3.py
+++ python/baekjoon/220414/13549_숨바꼭질3.py
@@ -16,25 +16,28 @@ q.append(N)   # 시작점 큐에 삽입
 visited[N] = 1  # 시작점 방문 체크
 dist[N] = 0     # 이동한 거리 표시
 
-# 43% 에서 실패
+# 44% 에서 실패
+# 해결 : 26번 줄 참고
 # 질문1 : 반례가 뭘까
-# 질문2 : 왜 dfs 는 안되는데, bfs는 높은 연산비용을 필요로하면서도 프로그램이 돌아가는걸까
+# big O 표기법 시간
+#    - 참고 : https://blog.yevgnenll.me/posts/calculate-recursive-big-o-number-of-steps-to-reduce-a-number-to-zero
 while q:
     cur = q.popleft()
+    # 순간이동을 제일 먼저 고려를 해줘야 이동 거리 줄일 수 있음, 고로 appendleft()활용해서 우선순위 조정.
     if cur*2 < 100001 and not visited[cur*2]:  # 해당 위치로 순간이동 가능하면
-        q.append(cur*2)
+        q.appendleft(cur*2)
         visited[cur*2] = 1
-        dist[cur*2] = dist[cur]             # 순간이동은 이동거리 +0
-    if cur + 1 < 100001 and not visited[cur+1]:
+        dist[cur*2] = dist[cur]                 # 순간이동은 이동거리 +0
+    if cur + 1 < 100001 and not visited[cur+1]:     # 해당 위치로 +1 걸어갈수 있으면
         q.append(cur+1)
         visited[cur+1] = 1
-        dist[cur+1] = dist[cur] + 1         # 걸으면 이동거리 +1
-    if cur - 1 >= 0 and not visited[cur-1]:
+        dist[cur+1] = dist[cur] + 1             # 걸으면 이동거리 +1
+    if cur - 1 >= 0 and not visited[cur-1]:     # # 해당 위치로 -1 걸어갈수 있으면
         q.append(cur-1)
         visited[cur-1] = 1
-        dist[cur-1] = dist[cur] + 1         # 걸으면 이동거리 +1
-    if dist[K] == K:
-        break
+        dist[cur-1] = dist[cur] + 1             # 걸으면 이동거리 +1
+    # if dist[K] == K:
+    #     break
 
 print(dist[K])
 
