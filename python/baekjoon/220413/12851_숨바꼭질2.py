c9966ebe8d4134c0963cd8793e1a0301dc794112
KIM Tae Hun <gift2barry@gmail.com>
2022-04-19 23:47:19 +0900

    algorithm: Fix 12851_숨바꼭질2 incomplete

--- python/baekjoon/220413/12851_숨바꼭질2.py
+++ python/baekjoon/220413/12851_숨바꼭질2.py
@@ -4,7 +4,6 @@
 # 수빈이가 동생을 찾을 수 있는 가장 빠른 시간
 # 그리고 가장 빠른 시간으로 찾는 방법이 몇가지인지 구하라      bfs
 
-# 2d 상에서 bfs 구현
 
 # 40분 소요
 # https://glowdev.tistory.com/70 출처 이해 불가
@@ -17,20 +16,52 @@ sys.stdin = open('input.txt')
 N, K = map(int, input().split())
 visited = [0] * 100001
 visited[N] = 1
-
-deq = deque()
-deq.append(N)
-
-time = 0
+dist = [-1] * 100001
+q = deque()
+dist[N] = 0
+q.append(N)
 
 cnt = 0
+min_num = 100000
+ans = deque()
+while q:
+    cur = q.popleft()
+    if dist[K] < 0:
+        if cur+1 < 100001 and not visited[cur+1]:
+            visited[cur+1] += 1
+            dist[cur+1] = dist[cur] + 1
+            q.append(cur+1)
+        if cur-1 >= 0 and not visited[cur-1]:
+            visited[cur-1] += 1
+            dist[cur-1] = dist[cur] + 1
+            q.append(cur-1)
+        if cur*2 < 100001 and not visited[cur*2]:
+            visited[cur*2] += 1
+            dist[cur*2] = dist[cur] + 1
+            q.append(cur*2)
+    if cur+1 == K or cur-1 == K or cur*2 == K:
+        cnt += 1
 
-while deq:
-    q = deq.popleft()
 
-    if q == K:
-
-
-    for i in q+1, q-1, q*2:
-        if not time and not visited[i] and i == K:
-            visited[i] = 1
+print(dist)
+print(visited)
+# while q:
+#     cur = q.popleft()
+#     if dist[K] < 0:
+#         if cur+1 < 100001:
+#             visited[cur+1] = 1
+#             dist[cur+1] = dist[cur] + 1
+#             q.append(cur+1)
+#         if cur-1 >= 0:
+#             visited[cur-1] = 1
+#             dist[cur-1] = dist[cur] + 1
+#             q.append(cur-1)
+#         if cur*2 < 100001:
+#             visited[cur*2] = 1
+#             dist[cur*2] = dist[cur] + 1
+#             q.append(cur*2)
+#     if cur+1 == K or cur-1 == K or cur*2 == K:
+#         cnt += 1
+print()
+print(dist[K])
+print(cnt)
