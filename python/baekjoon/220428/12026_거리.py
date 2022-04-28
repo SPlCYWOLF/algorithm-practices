95d46da77512f74934e048dc1a74c2b563d4bf6f
Tae Hun KIM <gift2barry@gmail.com>
2022-04-29 00:21:37 +0900

    algorithm: Fix 12026_거리 incomplete

--- python/baekjoon/220428/12026_거리.py
+++ python/baekjoon/220428/12026_거리.py
@@ -3,12 +3,33 @@
 import sys
 sys.stdin = open('input.txt')
 
-def dfs(b):
-    pass
+def dfs(b, cur, k):
+    global ans
+    if cur == N:
+        ans = min(ans, k)
 
+    for i in range(cur, N):
+        if b != 3 and blocks[i] == b + 1:
+            energy = (i+1) - cur
+            dfs(blocks[i], i+1, k + energy)
+        elif b == 3 and blocks[i] == 1:
+            energy = (i+1) - cur
+            dfs(blocks[i], i+1, k + energy)
+
+
+def converter(n):
+    if n == 'B':
+        return 1
+    elif n == 'O':
+        return 2
+    else:
+        return 3
 
 N = int(input())
 blocks = input()
-temp = [0] * N
+blocks = list(map(converter, blocks))
+temp = [0] * (N+1)
+ans = 1000
 
-dfs(blocks[0])
+dfs(blocks[0], 1, 0)    # 현재 블록 정보, 현재 위치, 누적된 이동 에너지
+print(ans)
