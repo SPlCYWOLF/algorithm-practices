443cf992223f0458379126f16ed65bfa67137520
KIM Tae Hun <gift2barry@gmail.com>
2022-07-06 01:22:42 +0900

    algorithm: Update 11725_트리의부모차기 incomplete

--- python/baekjoon/220704/11725_트리의부모찾기.py
+++ python/baekjoon/220704/11725_트리의부모찾기.py
@@ -3,20 +3,38 @@ sys.stdin = open('input.txt')
 
 
 N = int(input())
-tree = [[] for _ in range(N)]
+tree = dict()
+for i in range(N+1):
+    tree[i] = []
+print(tree)
 
 for i in range(N-1):
     parent, child = map(int, input().split())
-    if child in tree[1] or child == 1:
+    if child == 1 or 1 in tree[child]:
         parent, child = child, parent
-    tree[parent] += [child]
+    tree[child].append(parent)
 print(tree)
 
-ans = dict()
-for i in range(1, N):
-    for child in tree[i]:
-        ans.update({child: i})
-print(ans)
-
 for i in range(2, N+1):
-    print(ans[i])
+    print(*tree[i])
+
+
+
+# N = int(input())
+# tree = [[] for _ in range(N)]
+#
+# for i in range(N-1):
+#     parent, child = map(int, input().split())
+#     if child in tree[1] or child == 1:
+#         parent, child = child, parent
+#     tree[parent] += [child]
+# print(tree)
+#
+# ans = dict()
+# for i in range(1, N):
+#     for child in tree[i]:
+#         ans.update({child: i})
+# print(ans)
+#
+# for i in range(2, N+1):
+#     print(ans[i])
