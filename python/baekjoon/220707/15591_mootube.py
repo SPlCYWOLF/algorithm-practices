dba09ce54e8d876325fb5f6af60878290c69705a
Tae Hun KIM <gift2barry@gmail.com>
2022-07-27 17:59:04 +0900

    algorithm: Comment 15591 10021 solution logic

--- python/baekjoon/220707/15591_mootube.py
+++ python/baekjoon/220707/15591_mootube.py
@@ -2,29 +2,34 @@ import sys
 from collections import deque, defaultdict
 sys.stdin = open('input.txt')
 
-# 시간초과로 결국 통과 실패
+# USADO(유사도)로 연결된 동영상(노드)의 그래프를 bfs순회.
+# K(최소 유사도) 보다 높은곳을 방문.
+# K > 유사도인 경우 어차피 다음 경로로 이동 불가능, 고로 이동 경로상의 유사도의 최솟값 갱신 필요 x
+# 중복되는 쌍의 노드가 발생하지 않기때문에 싸이클 형성이 없으니 (노드개수 - 1)
+# union-find 알고리즘을 활용하여 상호 배타적 집합(Disjoint-set)을 찾음으로써
+# 노드의 싸이클 형성을 방지할 필요 x.
 def bfs(K, current_n):
-    visited = [0] * (N+1)
-    visited[current_n] = 1
+    visited = [0] * (N+1)       # 농부의 질문마다 방문 값 초기화
+    visited[current_n] = 1      # 현재 노드 위치 방문 처리
     Q = deque()
     Q.append((current_n, 99999999))
-    cnt = 0
+    cnt = 0                     # 추천되는 동영상 개수 축적
 
     while Q:
         current_n, temp_relevance = Q.popleft()
         for goal_n, relevance in graph[current_n]:
-            if relevance >= K and not visited[goal_n]:
+            if relevance >= K and not visited[goal_n]:  # 유사도 & 방문 검증
                 visited[goal_n] = 1
-                cnt += 1
+                cnt += 1        # 추천 되는 비디오 개수 추가
                 Q.append((goal_n, relevance))
-    return cnt
+    return cnt                  # 정답
 
 
 N, Q = map(int, input().split())
-graph = defaultdict(list)
+graph = defaultdict(list)   # key: 현재노드, val: (연결된 노드, 유사도)
 print(graph)
 
-for i in range(N-1):
+for i in range(N-1):        # 그래프 생성
     p, q, r = map(int, input().split())
     graph[p].append((q, r))
     graph[q].append((p, r))
@@ -33,6 +38,7 @@ print(graph)
 for i in range(Q):
     k, v = map(int, input().split())
     print(bfs(k, v))
+
 
 
 
@@ -71,18 +77,6 @@ for i in range(Q):
 # for i in range(Q):
 #     k, v = map(int, input().split())
 #     bfs(k, v)
-
-
-
-
-
-
-
-
-
-
-
-
 
 
 
--- python/baekjoon/220710/10021_wateringthefields.py
+++ python/baekjoon/220710/10021_wateringthefields.py
@@ -1,66 +1,70 @@
 import sys, heapq
 sys.stdin = open('input.txt')
 
-N, C = map(int, sys.stdin.readline().rstrip().split())
+N, C = map(int, sys.stdin.readline().rstrip().split())  # 연결하고 싶은 필드 개수, 최소 지불 비용
 
 # 시간초과
 # 크루스칼 알고리즘 활용하여 MST 찾기
 # distance값을 오름차순으로 정리 필요
 # heapq 활용하여 정렬
 # 완성된 heapq를 가지고 연결 가능한 간선값을 축적
-# cycle이 형성 안되는것을 확인하기 위해 union-find 알고리즘을 활용
+# cycle이 형성 안되는것을 확인하기 위해 union-find 알고리즘을 활용 : union(n1, n2)
+# n1과 n2가 최상위 부모노드를 공유한다면, 얘들이 연결되면 cycle이 형성되어버림
 # MST의 간선의 갯수는 N-1 임으로,
 # 간선의 갯수가 충족되면 distance의 총 합을, 반대의 경우는 -1을 반환
 
-def find(n):
+def find(n):            # 파인드 함수
     global root_table
 
-    if root_table[n] < 0:
+    if root_table[n] < 0:   # 부모노드가 없을 경우 자기 자신을 반환
         return n
-    else:
+    else:                   # 있을 경우 해당 부모노드의 부모노드 탐색
         root_table[n] = find(root_table[n])
-        return root_table[n]
+        return root_table[n]    # 최상위 부모노드 반환
 
-def union(n1, n2):
+def union(n1, n2):      # 유니온 함수
     global root_table
 
-    p_n1, p_n2 = find(n1), find(n2)
-    if p_n1 == p_n2:
-        return False
+    p_n1, p_n2 = find(n1), find(n2) # 각 노드에 대해 부모 노드 탐색
+    if p_n1 == p_n2:    # 부모 노드가 같을 시 cycle 형성되었다는 뜻,
+        return False    # 고로 false 반환
     else:
-        root_table[p_n2] = p_n1
+        root_table[p_n2] = p_n1     # p_n1를 p_n2의 부모노드로 설정
         return True
 
 temp = [0] * N
-for i in range(N):
+for i in range(N):      # 각 인풋 필드 위치 받아오기 : [x, y, 필드 식별 번호]
     temp[i] = list(map(int, sys.stdin.readline().rstrip().split())) + [i+1]
+print(temp)
 
-Q = []
+Q = []      # 크루스칼 알고리즘을 위해 최소 힙 자료구조로 건설비용(간선)기준 오름차순 정렬
 for i in range(N):
     x1, y1, node1 = temp[i]
     for j in range(i+1, N):
         x2, y2, node2 = temp[j]
-        d = abs(x1-x2)**2 + abs(y1-y2)**2
-        if d >= C:
+        d = abs(x1-x2)**2 + abs(y1-y2)**2   # 건설 비용
+        if d >= C:                          # 최소 지불 비용과 대조
             heapq.heappush(Q, (d, node1, node2))
+print(Q)
 
-if len(Q) < (N-1):
+if len(Q) < (N-1):  # MST의 조건(간선 개수 = 노드개수 - 1) 미달 시 컽
     print(-1)
-else:
-    root_table = [-1] * (N+1)
+else:               # MST일 가능성 있으면 진행
+    root_table = [-1] * (N+1)       # 각 노드의 최상위 부모 노드 기록용
     total_cost, road_cnt = 0, 0
 
-    while Q:
+    while Q:        # 최소 건설 비용부터 차례대로 꺼내서
         d, n1, n2 = heapq.heappop(Q)
-        if union(n1, n2):
-            total_cost += d
-            road_cnt += 1
+        if union(n1, n2):               # cycle 형성 안되는지 union-find 알고리즘으로 확인
+            total_cost += d             # 건설 비용 축적
+            road_cnt += 1               # 연결된 간선 개수 축적
+            # print(root_table)
         else:
-            if road_cnt + len(Q) < N-1:
+            if road_cnt + len(Q) < N-1:     # MST생성 가능성 없어지면 컽
                 print(-1)
                 break
 
-        if road_cnt == (N-1):       # 이걸 추가해서 통과
+        if road_cnt == (N-1):       # MST 완성(연결된 간선 개수 충족)되면 종료
             print(total_cost)
             break
 
