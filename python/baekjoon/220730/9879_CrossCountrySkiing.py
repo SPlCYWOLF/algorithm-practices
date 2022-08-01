7e27a542188777bb4ef8cbf9a6616002047511fe
Tae Hun KIM <gift2barry@gmail.com>
2022-08-01 21:11:00 +0900

    algorithm: Update 9879_crosscountryskiing incomplete

--- python/baekjoon/220730/9879_CrossCountrySkiing.py
+++ python/baekjoon/220730/9879_CrossCountrySkiing.py
@@ -4,4 +4,3 @@ sys.stdin = open('input.txt')
 M, N = map(int, input().split())
 elevations = [list(map(int, input().split())) for _ in range(M)]
 waypoint = [list(map(int, input().split())) for _ in range(M)]
-
