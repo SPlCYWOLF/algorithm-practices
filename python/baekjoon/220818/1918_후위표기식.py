a3026303ec3ae5f910088a56c9b3c04b8af8a4ae
Tae Hun KIM <gift2barry@gmail.com>
2022-08-18 17:06:50 +0900

    algorithm: Update 1918_후위표기식 incomplete

--- python/baekjoon/220818/1918_후위표기식.py
+++ python/baekjoon/220818/1918_후위표기식.py
@@ -7,92 +7,161 @@
 # 조건 :
 # 닫는 괄호를 만나면, 바로 지금까지 Q 출력 후 stack출력
 
+
+
 from collections import deque
 import sys
 sys.stdin = open('input.txt')
 
-given = input()
-ans = []
+# 괄호 열리면 bracket +1, 이때 만나는 연산자는 다 BQ에 넣음
+# 괄호 하나 닫힐때 마다 BQ에서 연산자 pop()
 
-Q, BQ = deque(), deque()
-stack, Bstack = [], []
-skip, bskip, bracket = False, False, 0
-for i in range(len(given)):
-    print(bracket)
-    if bracket:
-        if bskip:
-            bskip = False
+# 덧셈 or 나눗셈 나오면 다음칸 여는괄호인지 확인 후 여는괄호 아니면 바로 다음에 숫자 다음 출력 되도록 처리
+# 마지막에 남는 거 없이 다 출력
+
+for _ in range(5):
+
+    given = input()
+    ans = []
+    stack, Bstack = [], []
+    bracket = 0
+    go = False
+    for i in range(len(given)):
+
+        if go:
+            go = False
+            ans.append(given[i])
+            ans.append(Bstack.pop())
             continue
 
-        if given[i] == "*" or given[i] == "/":
-            bskip = True
-            BQ.append(given[i + 1])
-            Bstack.append(given[i])
-            while BQ:
-                ans.append(BQ.popleft())
-            while Bstack:
+        if bracket:
+
+            if given[i] == "*" or given[i] == "/":
+                if given[i+1] != "(":
+                    go = True
+                Bstack.append(given[i])
+                continue
+
+            if given[i] == "+" or given[i] == "-":
+                Bstack.append(given[i])
+                continue
+
+            if given[i] == ")" and Bstack:
+                bracket -= 1
                 ans.append(Bstack.pop())
-            continue
+                continue
 
-        if given[i] == ")":
-            while BQ:
-                ans.append(BQ.popleft())
-            while Bstack:
-                ans.append(Bstack.pop())
-            bracket -= 1
-            continue
 
-        if given[i] == "+" or given[i] == "-":
-            Bstack.append(given[i])
-            continue
 
         if given[i] == "(":
             bracket += 1
             continue
 
-        BQ.append(given[i])
-
-        continue
-
-    if skip:
-        skip = False
-        continue
-
-    if given[i] == "*" or given[i] == "/":
-        if bracket or given[i+1] == "(":
+        if given[i] == "*" or given[i] == "/":
+            if given[i+1] != "(":
+                go = True
             stack.append(given[i])
             continue
 
-        skip = True
-        Q.append(given[i+1])
-        stack.append(given[i])
-        while Q:
-            ans.append(Q.popleft())
-        while stack:
-            ans.append(stack.pop())
-        continue
+        if given[i] == "+" or given[i] == "-":
+            stack.append(given[i])
+            continue
 
-    if given[i] == "+" or given[i] == "-":
-        stack.append(given[i])
-        continue
+        if given[i] != "(":
+            ans.append(given[i])
 
-    if given[i] == "(":
-        bracket += 1
-        ans.append(Q.popleft())
-        continue
+    while stack:
+        ans.append(stack.pop())
 
-    Q.append(given[i])
-
-while Q:
-    ans.append(Q.popleft())
-while stack:
-    ans.append(stack.pop())
-
-print("".join(ans))
+    print("".join(ans))
 
 
 
 
+# 패작 2 : index error
+# given = input()
+# ans = []
+#
+# Q, BQ = deque(), deque()
+# stack, Bstack = [], []
+# skip, bskip, bracket = False, False, 0
+# for i in range(len(given)):
+#     if bracket:
+#         if bskip:
+#             bskip = False
+#             continue
+#
+#         if given[i] == "*" or given[i] == "/":
+#             bskip = True
+#             BQ.append(given[i + 1])
+#             Bstack.append(given[i])
+#             while BQ:
+#                 ans.append(BQ.popleft())
+#             while Bstack:
+#                 ans.append(Bstack.pop())
+#             continue
+#
+#         if given[i] == ")":
+#             while BQ:
+#                 ans.append(BQ.popleft())
+#             while Bstack:
+#                 ans.append(Bstack.pop())
+#             bracket -= 1
+#             continue
+#
+#         if given[i] == "+" or given[i] == "-":
+#             Bstack.append(given[i])
+#             continue
+#
+#         if given[i] == "(":
+#             bracket += 1
+#             continue
+#
+#         BQ.append(given[i])
+#
+#         continue
+#
+#     if skip:
+#         skip = False
+#         continue
+#
+#     if given[i] == "*" or given[i] == "/":
+#         if bracket or given[i+1] == "(":
+#             stack.append(given[i])
+#             continue
+#
+#         skip = True
+#         Q.append(given[i+1])
+#         stack.append(given[i])
+#         while Q:
+#             ans.append(Q.popleft())
+#         while stack:
+#             ans.append(stack.pop())
+#         continue
+#
+#     if given[i] == "+" or given[i] == "-":
+#         stack.append(given[i])
+#         continue
+#
+#     if given[i] == "(":
+#         bracket += 1
+#         ans.append(Q.popleft())
+#         continue
+#
+#     Q.append(given[i])
+#
+# while Q:
+#     ans.append(Q.popleft())
+# while stack:
+#     ans.append(stack.pop())
+#
+# print("".join(ans))
+
+
+
+
+
+# 패작 1 : 틀림
 # Q = deque()
 # stack = []
 # skip, bracket = False, 0
