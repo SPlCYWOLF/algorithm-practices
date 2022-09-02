4a0a3ecadcc3cfb0a4a17d4184f0d4836bddbff4
Tae Hun KIM <gift2barry@gmail.com>
2022-09-02 13:10:44 +0900

    algorithm: Update 1244_스위치켜고끄기 complete

--- python/baekjoon/220901/1244_스위치켜고끄기/1244_스위치켜고끄기.py
+++ python/baekjoon/220901/1244_스위치켜고끄기/1244_스위치켜고끄기.py
@@ -26,6 +26,8 @@ def sis(n):
                 switch[n+i] = 0
             else:
                 switch[n+i] = 1
+            continue
+        break       # 양옆 값 다르면 break
 
 N = int(input())
 switch = [0] + list(map(int, input().split()))
@@ -46,6 +48,45 @@ for i in range(1, N+1):
         print(switch[i])
     else:
         print(switch[i], end=" ")
+
+
+
+
+
+# def change(num):
+#     if switch[num] == 0:
+#         switch[num] = 1
+#     else:
+#         switch[num] = 0
+#     return
+#
+#
+# N = int(input())
+# switch = [-1] + list(map(int, input().split()))
+# students = int(input())
+# for _ in range(students):
+#     sex, num = map(int, input().split())
+#     print(switch)
+#     # 남자
+#     if sex == 1:
+#         for i in range(num, N + 1, num):
+#             change(i)
+#     # 여자
+#     else:
+#         change(num)
+#         for k in range(N // 2):
+#             if num + k > N or num - k < 1: break
+#             if switch[num + k] == switch[num - k]:
+#                 change(num + k)
+#                 change(num - k)
+#             else:
+#                 break
+#
+# for i in range(1, N + 1):
+#     print(switch[i], end=" ")
+#     if i % 20 == 0:
+#         print()
+
 
 # 정답 아웃풋 값 : 0 1 1 1 0 1 1 0
 
