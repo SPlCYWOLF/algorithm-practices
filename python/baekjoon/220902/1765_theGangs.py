ff8e371473e343f97ce1b4ee7de6c9e5b48a47d2
KIM Tae Hun <gift2barry@gmail.com>
2022-09-03 23:58:37 +0900

    algorithm: Update 1756_thegangs incomplete

--- python/baekjoon/220902/1765_theGangs.py
+++ python/baekjoon/220902/1765_theGangs.py
@@ -12,3 +12,6 @@ players = [tuple(map(converter, input().split())) for _ in range(M)]
 print(players)
 players = [0] + players + [0] * (N - len(players))
 print(players)
+
+for player in players:
+    print(player)
