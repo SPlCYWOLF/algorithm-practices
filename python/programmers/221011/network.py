0b2cb6aaf1ca948c09af13553d128826c0cded7b
KIM Tae Hun <gift2barry@gmail.com>
2022-10-11 20:59:21 +0900

    algorithm: Add programmers network complete

---
+++ python/programmers/221011/network.py
@@ -0,0 +1,23 @@
+# dfs로 방문하며 visited 체크.
+# dfs 종료되면 네트워크 개수 +1
+
+def dfs(cur, n, c, v):
+    for j in range(n):
+        if not v[j] and c[cur][j]:
+            v[j] = 1
+            dfs(j, n, c, v)
+
+
+def solution(n, computers):
+    visited = [0] * n
+    ans = 0
+
+    for i in range(n):
+        if not visited[i]:
+            visited[i] = 1
+            dfs(i, n, computers, visited)
+            ans += 1
+
+    return ans
+
+print(solution(3, [[1, 1, 0], [1, 1, 0], [0, 0, 1]]))
